5987fbbf3e855283683d26d6b307a92d0ed0a6f8
Sven Boenigk <xb1@live.de>
2018-06-14 15:07:23 +0200

    Now test_run-length is able to plot heatmaps and histograms

--- test_run-length_encoding.cpp
+++ test_run-length_encoding.cpp
@@ -4,9 +4,15 @@
 #include <cstdlib>
 #include <ctime>
 #include <climits>
+#include <fstream>
+#include <tgmath.h>
 using namespace std;
 using namespace seqan;
 
+#define HEIGHT 1080
+#define WIDTH 1920
+#define HISTROGRAM_SIZE 16
+#define BUCKET_WIDTH 15
 
 sdsl::bit_vector create_random_bit_v(int length){
 //     srand (time(0));
@@ -87,6 +93,87 @@ vector<char> encode(sdsl::bit_vector & bv, char limit){
     return(bv_encoded);
 }
 
+
+void heatmap(sdsl::bit_vector b){
+    int height = HEIGHT;
+    int width = WIDTH;
+    sdsl::rank_support_v<> rb(&b);
+    if(b.size() < height * width - 1){
+        cout << "changed image dimension to" << endl;
+        height = round(pow(static_cast<float>(b.size()) / 8 * 16 / 9, 0.5));
+        width = round(static_cast<float> (height) * 9 / 16);
+        cout << "height: " << height << " width: " << width << endl;
+    }
+    int window = floor(static_cast<float>(b.size()) / (height * width));
+    int pos = 0;
+        
+    ofstream img("heatmap.ppm");
+    img << "P3" << endl;
+    img << width << " " << height << endl;
+    img << "255" << endl;
+    for (int i = 0; i < height; ++i){
+        for(int j = 0; j < width; ++j){
+            float den = static_cast<float> (rb(pos + window) - rb(pos)) / window;
+            int r = round(den * 254);
+            int g = 0;
+            int b = 0;
+            img << r << " " << r << " " << r << endl;
+        }
+    }
+     system("eog heatmap.ppm");
+}
+
+vector<int> histogram(sdsl::bit_vector b ,const int his_size, const int bucket_width){
+    vector<int> hist(his_size, 0);
+    sdsl::bit_vector::iterator it = b.begin();
+    int n = 0;
+    while(it != b.end()){
+        while(*it == 0 && it != b.end()){
+            ++it;
+            ++n;
+        }
+        if(n != 0){
+            if(n < ((his_size - 1) * bucket_width))
+                hist[floor(n / bucket_width)] += 1;
+        }
+        n = 0;
+        if(it != b.end())
+            ++it; 
+    }
+    return(hist);
+}
+
+
+vector<int> histogram(sdsl::bit_vector b ,const int his_size){
+    vector<int> hist_lg(his_size, 0);
+    sdsl::bit_vector::iterator it = b.begin();
+    int n = 0;
+    while(it != b.end()){
+        while(*it == 0 && it != b.end()){
+            ++it;
+            ++n;
+        }
+        if(n != 0){
+             if(n < static_cast<int>(pow(2, his_size - 1))){
+                hist_lg[static_cast<int>(ceil(log2(static_cast<float>(n))))] += 1;
+             }
+        }
+        n = 0;
+        if(it != b.end())
+            ++it; 
+    }
+    return(hist_lg);
+}
+
+template<typename T>
+void print_vector(vector <T> v){
+    for(int i = 0; i < v.size(); ++i){
+        cout << v[i] << " ";
+    }
+    cout << "\n";
+}
+
+
 void dotest(vector<int> test){
     for(int i = 0; i < test.size(); ++i){
         sdsl::bit_vector rv = create_random_bit_v(test[i]);
@@ -126,9 +213,24 @@ void dotest(vector<int> test){
 int main(int argc, char *argv[])
 {
     vector<int> test = {10000, 100000, 1000000, 10000000, 100000000};
-    dotest(test);
+//     dotest(test);
+//     sdsl::bit_vector b = create_random_bit_v(10000000);
+    sdsl::bit_vector b;
+    load_from_file(b, argv[1]);
+    cout << "Load successful" << endl;
+    heatmap(b);
+    int his_size = HISTROGRAM_SIZE;
+    int bucket_width = BUCKET_WIDTH;
+
+    vector<int> hist = histogram(b, his_size, bucket_width);
+    vector<int> hist_lg = histogram(b, his_size);
+    print_vector(hist);
+    cout << "log" << endl;
+    print_vector(hist_lg);
     
     
+    
+
 
     //char test = -128; //127    
     
